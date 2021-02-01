94e60da97b6d2272ac589abcd76090b8f65a1b5b
anhtam.nguyen <anhtamit2019@gmail.com>
2021-02-01 17:40:51 +0700

    update check in out
    update check in out

--- routes/tam.php
+++ routes/tam.php
@@ -2,3 +2,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/dashboard', 'DashboardController@index');
+
+Route::get('/check-in-gps', 'CheckInOutController@index');
+
+Route::post('/check-in-gps', 'CheckInOutController@create');
