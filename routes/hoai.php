04dc107a28615e40e101dfbf4aa9ea9f2c8a1ee7
To Nam Son <tonamson@users.noreply.github.com>
2021-02-25 22:38:12 +0700

    Update hoai.php

--- routes/hoai.php
+++ routes/hoai.php
@@ -1,41 +1,22 @@
 <?php
-
-
 use Illuminate\Support\Facades\Route;
 
 //Department
-
 Route::group(['prefix' => 'deparment'], function () {
-    Route::get('/index', 'DepartmentController@index');
-    Route::get('/detail', 'DepartmentController@detailDep');
-    Route::get('/edit', 'DepartmentController@getEditDep');
-    Route::post('/add1', 'DepartmentController@CreateDepartment');
-    Route::get('/add', 'DepartmentController@add');
-    Route::post('/pedit', 'DepartmentController@postEditDep')->name('postEditDepartment');
+    Route::get('/index', 'DepartmentController@index')->name('getListDeparment');
+    Route::get('/detail', 'DepartmentController@detailDep')->name('detailDepartment');
+    Route::post('/add', 'DepartmentController@CreateDepartment')->name('postAddDepartment');
+    Route::get('/add', 'DepartmentController@add')->name('getAddDepartment');
+    Route::get('/edit', 'DepartmentController@getEditDep')->name('getEditDepartment');
+    Route::post('/edit', 'DepartmentController@postEditDep')->name('postEditDepartment');
 });
 
 //Staff
-
 Route::group(['prefix' => 'staff'], function () {
     Route::get('/index', 'StaffController@index');
-});
-
-Route::group(['prefix' => 'staff'], function () {
     Route::get('/add', 'StaffController@vaddStaff');
-});
-
-Route::group(['prefix' => 'staff'], function () {
-    Route::post('/add', 'StaffController@CreateStaff')->name('postAddStaff');
-});
-
-Route::group(['prefix' => 'deparment'], function () {
     Route::get('/detail', 'StaffController@getDetail');
-});
-
-Route::group(['prefix' => 'deparment'], function () {
     Route::get('/gedit', 'StaffController@getEditStaff');
-});
-
-Route::group(['prefix' => 'deparment'], function () {
     Route::post('/pedit', 'StaffController@postEditStaff');
+    Route::post('/add', 'StaffController@CreateStaff')->name('postAddStaff');
 });
