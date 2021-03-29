2df1b582586bafa3100087c95e06bd332616e528
To Nam Son <tonamson@users.noreply.github.com>
2021-03-29 20:39:43 +0700

    Update create.blade.php

--- resources/views/main/contract/create.blade.php
+++ resources/views/main/contract/create.blade.php
@@ -74,20 +74,6 @@
                             </div>
                         </div>
                         <div class="row">
-                            <div class="col-md-6">
-                                <div class="form-group">
-                                    <label>Hộ khẩu thường trú:</label>
-                                    <input type="text" class="form-control" name="address1" value="">
-                                </div>
-                            </div>
-                            <div class="col-md-6">
-                                <div class="form-group">
-                                    <label>Nơi ở hiện tại:</label>
-                                    <input type="text" class="form-control" name="address2" value="">
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row">
                             <div class="col-md-12">
                                 <div class="form-group">
                                     <label>Lương cơ bản:</label>
