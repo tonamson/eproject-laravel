816106c0046c4b3b69c8c6c4d514e090d6b5e5f5
anhtam.nguyen <anhtamit2019@gmail.com>
2021-04-05 17:17:42 +0700

    Update mail.php

--- config/mail.php
+++ config/mail.php
@@ -9,5 +9,12 @@ return [
     'username' => env("MAIL_USERNAME"),
     'password' => env("MAIL_PASSWORD"),
     'sendmail' => '/usr/sbin/sendmail -bs',
-    'pretend' => false
+    'pretend' => false,
+    'stream' => [
+        'ssl' => [
+           'allow_self_signed' => true,
+           'verify_peer' => false,
+           'verify_peer_name' => false,
+        ],
+     ],
 ];
