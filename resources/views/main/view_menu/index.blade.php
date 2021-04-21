4097ce86ec9608b306476cadfa1aa9fbc658ec0e
RedPCHome <anhtamit2019@gmail.com>
2021-04-21 16:10:54 +0700

    update

--- resources/views/main/view_menu/index.blade.php
+++ resources/views/main/view_menu/index.blade.php
@@ -357,14 +357,16 @@
             </div>
         @endif
 
-        <div class="col-6 col-md-3 text-center col-max">
-            <div class="outer">
-                <a href="{{ route('mySalary', ['id' => auth()->id()]) }}">
-                    <i class="icon-8x icon-cash3"></i>
-                    <p>Bảng Lương Cá Nhân</p> 
-                </a>
+        @if(auth()->user()->id != 7 || auth()->user()->department != 5)
+            <div class="col-6 col-md-3 text-center col-max">
+                <div class="outer">
+                    <a href="{{ route('mySalary', ['id' => auth()->id()]) }}">
+                        <i class="icon-8x icon-cash3"></i>
+                        <p>Bảng Lương Cá Nhân</p> 
+                    </a>
+                </div>
             </div>
-        </div>
+        @endif
 
         {{-- <div class="col-6 col-md-3 text-center col-max">
             <div class="outer">
